3a154a5ed6c29617f1530d8612f3c65a4978a269
Léo <leonardo_carvalho@outlook.com>
2016-02-06 19:58:45 -0200

    Tests completed

--- src/main/java/br/com/leo/agenda/dao/ContadoDAO.java
+++ src/main/java/br/com/leo/agenda/dao/ContadoDAO.java
@@ -1,7 +1,11 @@
 package br.com.leo.agenda.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import br.com.leo.agenda.connection.DBConnection;
 import br.com.leo.agenda.entity.Contato;
@@ -27,6 +31,68 @@ public class ContadoDAO {
 		}
 	}
 
+	public boolean delete(Integer id){
+		Session session = DBConnection.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try{
+			transaction = session.beginTransaction();
+			session.delete(findById(id));
+			transaction.commit();
+			return true;
+		}catch(RuntimeException e){
+			if(transaction != null){
+				transaction.rollback();
+				return false;
+			}
+			throw e;
+		}finally{
+			session.close();
+		}
+	}
 	
+	public boolean update(Contato c){
+		Session session = DBConnection.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try{
+			transaction = session.beginTransaction();
+			session.update(c);
+			transaction.commit();
+			return true;
+		}catch(RuntimeException e){
+			if(transaction != null){
+				transaction.rollback();
+				return false;
+			}
+			throw e;
+		}finally{
+			session.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Contato> listAll(){
+		Session session = DBConnection.getSessionFactory().openSession();
+		try{
+			Criteria result = session.createCriteria(Contato.class);
+			return result.list();
+		}catch(RuntimeException e){
+			throw e;
+		}finally{
+			session.close();
+		}
+	}
+	
+	public Contato findById(Integer id){
+		Session session = DBConnection.getSessionFactory().openSession();
+		try{
+			Criteria result = session.createCriteria(Contato.class);
+			result.add(Restrictions.idEq(id));
+			return (Contato) result.uniqueResult();
+		}catch(RuntimeException e){
+			throw e;
+		}finally{
+			session.close();
+		}
+	}
 	
 }
